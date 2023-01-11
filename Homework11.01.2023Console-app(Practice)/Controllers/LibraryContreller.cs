a3c5f7d16698e86a738b890fbcaa809dc3515f45
Alakbar Hasanov <alakbarbh@code.edu.az>
2023-01-11 23:53:48 +0400

    C#Homework 11.01.2023(Console app practice)

---
+++ Homework11.01.2023Console-app(Practice)/Controllers/LibraryContreller.cs
@@ -0,0 +1,66 @@
+﻿using DomainLayer.Entities;
+using ServiceLayer.Helpers;
+using ServiceLayer.Services;
+using ServiceLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11._01._2023Console_app_Practice_.Controllers
+{
+    public class LibraryContreller
+    {
+        private readonly ILibrarySevice _libraryService;
+
+        public LibraryContreller()
+        {
+            _libraryService = new LibraryService();
+        }
+
+        public void Create()
+        {
+            ConsoleColor.DarkCyan.WriteConsole("Please add library name:");
+            LibraryName: string libraryName = Console.ReadLine();
+
+            ConsoleColor.DarkCyan.WriteConsole("Please add library seat count");
+            SeatCount: string seatCountStr = Console.ReadLine();
+
+            int seatCount;
+
+            bool isCorrectSeatCount = int.TryParse(seatCountStr, out seatCount);
+
+            if (isCorrectSeatCount)
+            {
+                try
+                {
+
+                    Library library = new Library
+                    {
+                        Name = libraryName,
+                        SeatCount = seatCount
+                    };
+
+                    var response = _libraryService.Create(library);
+
+                    ConsoleColor.Green.WriteConsole($"{response.Id} {response.Name} {response.SeatCount}");
+                }
+                catch (Exception ex)
+                {
+
+                    ConsoleColor.Red.WriteConsole(ex.Message + "/" + "Please try again");
+                    goto LibraryName;
+                }
+
+
+            }
+            else
+            {
+                ConsoleColor.Red.WriteConsole("Please add correct format seat count");
+                goto SeatCount;
+            }
+
+        }
+    }
+}
---
+++ Homework11.01.2023Console-app(Practice)/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+using Homework11._01._2023Console_app_Practice_.Controllers;
+using ServiceLayer.Helpers;
+
+LibraryContreller LibraryContreller = new();
+
+while (true)
+{
+    GetOptions();
+
+    Option: string option = Console.ReadLine();
+    int selectedOption;
+
+    bool isCorrectOPtion = int.TryParse(option, out selectedOption);
+
+    if (isCorrectOPtion)
+    {
+        switch (selectedOption)
+        {
+            case 1:
+                LibraryContreller.Create();
+                break;
+
+            case 2:
+                Console.WriteLine("Get all");
+                break;
+            case 3:
+                Console.WriteLine("Delete");
+                break;
+            default:
+                ConsoleColor.Red.WriteConsole("Please add correct option");
+                goto Option;
+                
+        }
+    }
+    else
+    {
+        ConsoleColor.Red.WriteConsole("Please add correct format option");
+        goto Option;
+    }
+
+}
+
+static void GetOptions()
+{
+    ConsoleColor.Cyan.WriteConsole("Please select one option: ");
+    ConsoleColor.Cyan.WriteConsole("Library options: 1 - Create, 2 - Get all, 3 - Delete");
+}
